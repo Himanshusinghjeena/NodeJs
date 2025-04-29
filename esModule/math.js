90b07cd90f2e92527b1942d9fd41b6f70b4503db
Himanshusinghjeena <114165565+Himanshusinghjeena@users.noreply.github.com>
2025-04-29 23:15:38 +0530

    29 APRIL

---
+++ esModule/math.js
@@ -0,0 +1,10 @@
+// import mul from './app.js';
+
+
+import {add,sub,mul,div} from './app.js'
+import *  as math from './app.js'
+
+console.log("add",math.add(2,3));
+console.log("sub",sub(2,3));
+console.log("mul",math.mul(2,3));
+console.log("div",div(2,3));
---
+++ http_module/server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+// if i do any changes in this server the result would not reflect we have to restart the server manually 
+const server = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.setHeader('content-Type',"text/html")
+
+        res.write("<h1> I am Himanshu Singh Jeena aka HSJ Pahadi himanshu hgsfhkfahk hhhh </h1>");
+        res.end()
+    }
+    // Source Code
+    if(req.url === '/source-code'){
+        res.write("It is the source code page for source cod ewrite by yourself");
+        res.end()
+    }
+    // Contact Page
+    if(req.url === '/contact'){
+        res.setHeader('content-Type',"text/plain")
+        res.write("DO you want to work with me or Collabrate with me");
+        res.end()
+    }
+});
+// server is also a event emmitter 
+
+
+const PORT=3000;;
+server.listen(PORT,()=>{
+    console.log(`Listening on Port ${PORT}`)
+})
